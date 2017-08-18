39780eed109b4ca857c379ec3d6191aa8ac20549
Yuan Zhao <yuanzhaoYZ@users.noreply.github.com>
2017-08-18 08:20:37 -0500

    Update bruteforce.py

--- bruteforce.py
+++ bruteforce.py
@@ -10,7 +10,7 @@ rarfile.UNRAR_TOOL="unrar"
 def bruteforce(charset,minLength,maxlength):
     return (''.join(candidate)
         for candidate in itertools.chain.from_iterable(itertools.product(charset, repeat=i)
-        for i in range(1, maxlength + 1)))
+        for i in range(minLength, maxlength + 1)))
 def extractFile(arFile, attempt):
     try:
         arFile.extractall(pwd=attempt)
